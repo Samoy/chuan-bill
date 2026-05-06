26917294ec7d4dcd4a4f875b47c453eafce3414b
samoy <samoy_young@163.com>
2026-05-06 22:42:39 +0800

    style(page): 添加页面底部安全区域适配

--- chuan-bill-app/src/App.vue
+++ chuan-bill-app/src/App.vue
@@ -7,6 +7,7 @@ onLaunch(() => {})
   min-height: calc(100vh - var(--window-top));
   box-sizing: border-box;
   background: #faf8fc;
+  padding-bottom: env(safe-area-inset-bottom);
 }
 
 .wot-theme-dark.page-wraper {
