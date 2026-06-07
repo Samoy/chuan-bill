fe77f69d1aff9dddb672bf2f7f5e685a33fb6362
samoy <samoy_young@163.com>
2026-06-07 21:25:30 +0800

    test(server): 移除 ChuanBillServerApplicationTests 类中的无用注解
    - 删除了 @SpringBootTest 注解
    - 简化了测试类结构
    - 保留了测试方法但未添加实现内容

--- chuan-bill-server/src/test/java/com/samoy/chuanbillserver/ChuanBillServerApplicationTests.java
+++ chuan-bill-server/src/test/java/com/samoy/chuanbillserver/ChuanBillServerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.samoy.chuanbillserver;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ChuanBillServerApplicationTests {
 
     @Test
