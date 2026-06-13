f9b77b5bd7a6b609e1e60a048d51b699cec258b8
samoy <samoy_young@163.com>
2026-06-13 15:26:40 +0800

    feat(payment): 实现支付方式 CRUD 和排序服务方法

--- chuan-bill-server/src/main/java/com/samoy/chuanbillserver/service/impl/PaymentMethodServiceImpl.java
+++ chuan-bill-server/src/main/java/com/samoy/chuanbillserver/service/impl/PaymentMethodServiceImpl.java
@@ -1,13 +1,22 @@
 package com.samoy.chuanbillserver.service.impl;
 
+import cn.hutool.core.util.IdUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.samoy.chuanbillserver.dao.BillMapper;
 import com.samoy.chuanbillserver.dao.PaymentMethodMapper;
+import com.samoy.chuanbillserver.dto.AddPaymentMethodDTO;
+import com.samoy.chuanbillserver.dto.UpdatePaymentMethodDTO;
+import com.samoy.chuanbillserver.entity.Bill;
 import com.samoy.chuanbillserver.entity.PaymentMethod;
+import com.samoy.chuanbillserver.exception.BusinessException;
+import com.samoy.chuanbillserver.result.ResultEnum;
 import com.samoy.chuanbillserver.service.IPaymentMethodService;
 import com.samoy.chuanbillserver.vo.PaymentMethodVO;
+import jakarta.annotation.Resource;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p>
@@ -20,6 +29,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentMethodServiceImpl extends ServiceImpl<PaymentMethodMapper, PaymentMethod>
         implements IPaymentMethodService {
+
+    @Resource
+    private BillMapper billMapper;
+
     @Override
     public List<PaymentMethodVO> getPaymentMethods(String userId) {
         LambdaQueryWrapper<PaymentMethod> queryWrapper = new LambdaQueryWrapper<PaymentMethod>()
@@ -40,5 +53,101 @@ public class PaymentMethodServiceImpl extends ServiceImpl<PaymentMethodMapper, P
                     return vo;
                 })
                 .toList();
+    }
+
+    @Override
+    @Transactional
+    public PaymentMethodVO addPaymentMethod(String userId, AddPaymentMethodDTO dto) {
+        LambdaQueryWrapper<PaymentMethod> maxQuery = new LambdaQueryWrapper<>();
+        maxQuery.eq(PaymentMethod::getUserId, userId);
+        List<PaymentMethod> existing = this.list(maxQuery);
+        int maxSortOrder =
+                existing.stream().mapToInt(PaymentMethod::getSortOrder).max().orElse(0);
+
+        PaymentMethod paymentMethod = new PaymentMethod();
+        paymentMethod.setId(IdUtil.fastSimpleUUID());
+        paymentMethod.setName(dto.getName());
+        paymentMethod.setIcon(dto.getIcon());
+        paymentMethod.setSortOrder(maxSortOrder + 1);
+        paymentMethod.setIsDefault(false);
+        paymentMethod.setUserId(userId);
+        paymentMethod.setDeleted(false);
+        this.save(paymentMethod);
+
+        return toVO(paymentMethod);
+    }
+
+    @Override
+    @Transactional
+    public PaymentMethodVO updatePaymentMethod(String userId, String id, UpdatePaymentMethodDTO dto) {
+        PaymentMethod paymentMethod = this.getById(id);
+        if (paymentMethod == null || Boolean.TRUE.equals(paymentMethod.getDeleted())) {
+            throw new BusinessException(ResultEnum.PAYMENT_METHOD_NOT_FOUND);
+        }
+        if (Boolean.TRUE.equals(paymentMethod.getIsDefault())) {
+            throw new BusinessException(ResultEnum.CANNOT_MODIFY_DEFAULT_PAYMENT_METHOD);
+        }
+        if (!userId.equals(paymentMethod.getUserId())) {
+            throw new BusinessException(ResultEnum.PAYMENT_METHOD_NOT_FOUND);
+        }
+
+        if (dto.getName() != null) {
+            paymentMethod.setName(dto.getName());
+        }
+        if (dto.getIcon() != null) {
+            paymentMethod.setIcon(dto.getIcon());
+        }
+        this.updateById(paymentMethod);
+
+        return toVO(paymentMethod);
+    }
+
+    @Override
+    @Transactional
+    public void deletePaymentMethod(String userId, String id) {
+        PaymentMethod paymentMethod = this.getById(id);
+        if (paymentMethod == null || Boolean.TRUE.equals(paymentMethod.getDeleted())) {
+            throw new BusinessException(ResultEnum.PAYMENT_METHOD_NOT_FOUND);
+        }
+        if (Boolean.TRUE.equals(paymentMethod.getIsDefault())) {
+            throw new BusinessException(ResultEnum.CANNOT_MODIFY_DEFAULT_PAYMENT_METHOD);
+        }
+        if (!userId.equals(paymentMethod.getUserId())) {
+            throw new BusinessException(ResultEnum.PAYMENT_METHOD_NOT_FOUND);
+        }
+
+        LambdaQueryWrapper<Bill> billQuery = new LambdaQueryWrapper<>();
+        billQuery.eq(Bill::getPaymentMethodId, id).last("LIMIT 1");
+        if (billMapper.selectCount(billQuery) > 0) {
+            throw new BusinessException(ResultEnum.PAYMENT_METHOD_HAS_BILLS);
+        }
+
+        this.removeById(id);
+    }
+
+    @Override
+    @Transactional
+    public void sortPaymentMethods(String userId, List<String> ids) {
+        int sortOrder = 1;
+        for (String id : ids) {
+            PaymentMethod paymentMethod = this.getById(id);
+            if (paymentMethod != null
+                    && userId.equals(paymentMethod.getUserId())
+                    && Boolean.FALSE.equals(paymentMethod.getIsDefault())) {
+                paymentMethod.setSortOrder(sortOrder++);
+                this.updateById(paymentMethod);
+            }
+        }
+    }
+
+    private PaymentMethodVO toVO(PaymentMethod paymentMethod) {
+        PaymentMethodVO vo = new PaymentMethodVO();
+        vo.setId(paymentMethod.getId());
+        vo.setName(paymentMethod.getName());
+        vo.setIcon(paymentMethod.getIcon());
+        vo.setSortOrder(paymentMethod.getSortOrder());
+        vo.setIsDefault(paymentMethod.getIsDefault());
+        vo.setUserId(paymentMethod.getUserId());
+        return vo;
     }
 }
