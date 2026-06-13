1c00b87ada414162116ea485c37cc5a65646cf8f
samoy <samoy_young@163.com>
2026-06-13 15:20:28 +0800

    fix(category): 为 SortDTO 添加 @Schema 注解

--- chuan-bill-server/src/main/java/com/samoy/chuanbillserver/dto/SortDTO.java
+++ chuan-bill-server/src/main/java/com/samoy/chuanbillserver/dto/SortDTO.java
@@ -1,11 +1,14 @@
 package com.samoy.chuanbillserver.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.Data;
 
 @Data
+@Schema(description = "排序请求")
 public class SortDTO {
 
-    @NotEmpty(message = "排序列表不能为空") private List<String> ids;
+    @NotEmpty(message = "排序列表不能为空") @Schema(description = "排序后的ID列表", requiredMode = Schema.RequiredMode.REQUIRED)
+    private List<String> ids;
 }
